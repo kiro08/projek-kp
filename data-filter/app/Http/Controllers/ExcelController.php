d71ec0001b5a99e4261a6c46f3d8bb1484eb66ee
Afranfaiq <afraayinfaiq@gmail.com>
2023-06-22 16:17:23 +0700

    import bisa baca bersheet

--- data-filter/app/Http/Controllers/ExcelController.php
+++ data-filter/app/Http/Controllers/ExcelController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Illuminate\Support\Facades\Auth; 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Hash; 
@@ -29,13 +31,25 @@ class ExcelController extends Controller
         ]);
 
         // Membaca file Excel
-        $data = Excel::toCollection(new ExcelImport, $file);
+        $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($file);
+        $sheetNames = $spreadsheet->getSheetNames();
 
-        // Mengambil nama kolom dari file Excel
-        $columnNames = $data[0]->first()->toArray();
+        // Iterasi melalui setiap sheet
+        foreach ($sheetNames as $index => $sheetName) {
+            $sheet = $spreadsheet->getSheetByName($sheetName);
 
-        // Menentukan nama tabel berdasarkan inputan user
-        $tableName = $request->input('table_name');
+            // Mengambil nama kolom dari sheet
+            $columnNames = [];
+            $cellIterator = $sheet->getRowIterator(1)->current()->getCellIterator();
+            $cellIterator->setIterateOnlyExistingCells(true); // Menghindari sel yang kosong
+            foreach ($cellIterator as $cell) {
+                $columnNames[] = $cell->getValue();
+    }
+
+
+        // Menentukan nama tabel berdasarkan inputan dari user dengan tambahan angka urutan
+        $tableName = $request->input('table_name') . '_' . ($index + 1);
+
 
         // Membuat tabel baru berdasarkan nama tabel yang ditentukan
         Schema::create($tableName, function ($table) use ($columnNames) {
@@ -45,21 +59,28 @@ class ExcelController extends Controller
         });
 
         // Insert data into the newly created table
-        $rows = $data[0]->skip(1); // Skip the first row (column names)
+        $rows = $sheet->toArray();
         $insertData = [];
-        foreach ($rows as $row) {
+        foreach ($rows as $rowIndex => $row) {
+            if ($rowIndex === 0) {
+                // Skip the first row (column names)
+                continue;
+            }
+
             $rowData = [];
-            foreach ($columnNames as $index => $columnName) {
+            foreach ($columnNames as $columnIndex => $columnName) {
                 // Pastikan data yang akan dimasukkan ke kolom sesuai dengan struktur tabel
-                $rowData[$columnName] = $row[$index] ?? null;
+                $rowData[$columnName] = $row[$columnIndex] ?? null;
             }
             $insertData[] = $rowData;
         }
 
         DB::table($tableName)->insert($insertData);
-
-        return redirect()->back()->with('success', 'File Excel berhasil diunggah dan disimpan.');
     }
+
+    return redirect()->back()->with('success', 'File Excel berhasil diunggah dan disimpan.');
+}
+
 
     public function listTables()
     {
