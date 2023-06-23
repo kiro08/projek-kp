fc764da41d0c48a789509da71b00484a2a742485
Afranfaiq <afraayinfaiq@gmail.com>
2023-06-23 13:34:56 +0700

    Fix ExcelController

--- data-filter/resources/views/Excel/index.blade.php
+++ data-filter/resources/views/Excel/index.blade.php
@@ -47,8 +47,8 @@
         <form method="POST" action="{{ route('store') }}" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
-                <input type="file" name="excel_file" class="form-control-file">
-                @error('excel_file')
+                <input type="file" name="excel_files[]" class="form-control-file" multiple>
+                @error('excel_files')
                     <div class="alert alert-danger mt-2">{{ $message }}</div>
                 @enderror
             </div>
@@ -56,8 +56,13 @@
                 <label for="table_name" class="form-label">Nama Tabel</label>
                 <input type="text" class="form-control" id="table_name" name="table_name" required>
             </div>
+            <div class="mb-3">
+                <label for="reference_column" class="form-label">Kolom Acuan</label>
+                <input type="text" class="form-control" id="reference_column" name="reference_column" required>
+            </div>
             <button type="submit" class="btn btn-primary">Unggah</button>
         </form>
+
     </div>
 </body>
 </html>
